194ab674512d18d2af44fba7c1bb1cad3d5d4c35
s162338 CallanD <callan.davies1@gmail.com>
2017-05-16 11:14:59 +1000

    Constructor, Deconstrucor and Private Variable in LinkedList

--- DynamicArray-LinkedList/LinkedList.h
+++ DynamicArray-LinkedList/LinkedList.h
@@ -1,12 +1,37 @@
 #pragma once
-
 #include "Node.h"
 
 
 template <class T>
 class LinkedList 
 {
+private:
+	//Sentinel nodes
+	Node<T>* m_start;
+	Node<T>* m_end;
 
+public:
+	//Constructor
+	LinkedList()
+	{
+		m_start = new Node<T>();
+		m_end = new Node<T>();
+
+		//Link to eachother
+		m_start->m_Next = m_end;
+		m_start->m_Previous = nullptr;
+
+		m_end->m_Next = nullptr;
+		m_end->m_Previous = m_start;
+	}
+
+	~LinkedList()
+	{
+		delete m_start;
+		m_start = nullptr;
+		delete m_end;
+		m_end = nullptr;
+	}
 
 
 	void Insert(T a_data, int a_iIndex)
