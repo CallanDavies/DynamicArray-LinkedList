8dfd697a3a315e73e82b644e64abba6559481d48
s162338 CallanD <callan.davies1@gmail.com>
2017-06-06 10:59:10 +1000

    Almost finished LinkedList, just a couple more functions till completion

--- DynamicArray-LinkedList/LinkedList.h
+++ DynamicArray-LinkedList/LinkedList.h
@@ -1,163 +1,170 @@
 #pragma once
-#include "Node.h"
-
 
 template <class T>
 class LinkedList
 {
-private:
-	//Sentinel nodes
-	Node<T>* m_start;
-	Node<T>* m_end;
-
 public:
-	//Constructor
-	LinkedList()
+	template <class T>
+	struct Node
 	{
-		m_start = new Node<T>();
-		m_end = new Node<T>();
+		// Data found in this node
+		T value;
+		//Pointer to next item
+		Node* next;
+		//Pointer to previous item
+		Node* previous;
+	};
 
-		//Link to eachother
-		m_start->m_Next = m_end;
-		m_start->m_Previous = nullptr;
+	class Iterator
+	{
+	public:
+		Iterator() {}
+		Iterator(Node<T>* node) {}
+		~Iterator() {}
 
-		m_end->m_Next = nullptr;
-		m_end->m_Previous = m_start;
+		bool operator == (const Iterator& other)
+		{
+			return m_node == other.m_node;
+		}
+
+		bool operator != (const Iterator& other)
+		{
+			return m_node != other.m_node;
+		}
+
+		Iterator operator++ ()
+		{
+			m_node = m_node->next;
+			return *this;
+		}
+
+		Iterator operator ++ (int)
+		{
+			Iterator temp(m_node);
+			m_node = m_node->next;
+			return temp;
+		}
+		// decrement operator
+		Iterator operator -- ()
+		{
+			Iterator temp(m_node);
+			m_node = m_node->previous;
+			return temp;
+		}
+
+
+		// returns the value of items in the list
+		T& get(){return m_node->value; }
+
+		T& operator * () { return m_node->value; }
+		T& operator -> () { return &(m_node->value); }
+	private:
+		Node<T>* m_node = nullptr;
+	};
+
+	// return an iterator to the first element
+	Iterator begin() { return Iterator(m_first); }
+
+	// return an interator to a null element
+	Iterator end() { return Iterator(m_last); }
+	
+	//remove an element by its iterator
+	void erase(Iterator a_iterator)
+	{
+
 	}
 
-	~LinkedList()
-	{
-		delete m_start;
-		m_start = nullptr;
-		delete m_end;
-		m_end = nullptr;
-	}
 
-
-
-	// Add a new value(a_data) to the front of the list
-	void PushFront(T* a_data)
-	{
-
-	}
-    
-	// Add a new value(a_data) to the end of the list
-	void PushBack(T* a_data)
-	{
-
-	}
-
-	// Add a new value(a_data) one-past the specified iterator location(a_iIndex)
-	void Insert(T a_data, int a_iIndex)
+	// add a new value one-past the specigied iterator location
+	void Insert(T a_value, int a_iIndex)
 	{
 		// Step through and creat new node, linking 
 		// and setting value
 		int iCount = 0;
-		Node<T>* current = m_start->m_Next;
+		Node<T>* current = m_first->next;
 		while (current != m_end && iCount != a_iIndex)
 		{
 			++iCount;
-			current = current->m_Next;
+			current = current->next;
 		}
 
 		Node<T>* temp = new Node<T>();
-		temp->m_data = a_data;
+		temp->value = a_value;
 
 		// linking 
-		temp->m_Next = current;
-		temp->m_Previous = current->m_Previous;
-		current->m_Previous->m_Next = temp;
-		current->m_Previous = temp;
+		temp->next = current;
+		temp->previous = current->previous;
+		current->previous->next = temp;
+		current->previous = temp;
 	}
 
-	// return an iterator to the first element
-	int Begin()
+	LinkedList() {}
+	LinkedList (int startingElements) : m_elements(startingElements)
 	{
-
-	}
-
-	// return an iterator to a null element
-	int End()
-	{
-		
-	}
-
-	// return the first element by value, assert if no elements
-	T* First()
-	{
-		if (Empty)
+		// Loop through the new list nodes
+		for (int i = 0; i < m_elements; ++i)
 		{
+			Node* node = new Node();
+			if (i == 0)
+			{
+				m_last = node;
+				// make it point to the first item
+				node->next = m_first;
+			}
 			
+			if (m_first != nullptr)
+			{
+				m_first->previous = node;
+			}
 		}
-
 	}
 
-	// return the last element by value, assert if no elements
-	T* Last()
+	~LinkedList()
 	{
-		if (Empty)
-		{
-			
-		}
-
 	}
 
-	// return how many elements exist in the list
-	int Count()
-	{
 
-	}
+	//return how many elements exist in the list
+	int count() { return m_elements; }
 
-	// remove an element by its iterator(a_iIndex)
-	void Erase(int a_iIndex)
-	{
+	//return the value of the first element
+	T* first() { return m_first->value; }
 
-	}
-	// remove all elements with matching value(a_data)
-	void Remove(T* a_data)
-	{
+	// return the value of the first element
+	T* last() { return m_last->value; }
 
-	}
-
-	// remove the last element
-	void PopBack()
-	{
-
-	}
-
-	//remove the first element
-	void PopFront()
+	// add a new value to the end of the list
+	void push_back(const T & value)
 	{
 
 	}
 
 	// return a Boolean, true if the list is empty, false otherwise
-	bool Empty()
+	bool empty()
 	{
-		if (m_start == nullptr)
+		if (m_elements == 0)
 		{
 			return true;
 		}
 		return false;
 	}
 
-	//Clear all nodes from the list
-	void Clear()
+	//remove all elements from the list
+	void clear()
 	{
-		for (int i = 0; i < m_end; ++i)
-		{
 
-		}
 	}
+
+	// remove all elements with matching value
+	void remove(T* a_value)
+	{
+
+	}
+	private:
+
+		//Sentinel nodes
+		Node<T>* m_first = nullptr;
+		Node<T>* m_last = nullptr;
+
+		int m_elements = 0;
 };
-
-
-
-
-
-
-
-
-
-
-
--- DynamicArray-LinkedList/Source.cpp
+++ DynamicArray-LinkedList/Source.cpp
@@ -1,9 +1,18 @@
+#include <iostream>
 
-#include "DynamicArray.h"
 #include "LinkedList.h"
 
 void main()
 {
-	
+	LinkedList<int>myList;
+	LinkedList<int>myList2(10);
+
+//	myList2.push_back(22);
+//	myList2.push_front(25);
+
+	for (auto i = myList.begin(); i != myList.end(); ++i)
+	{
+		std::cout << i.get() << std::endl;
+	}
 
 }
